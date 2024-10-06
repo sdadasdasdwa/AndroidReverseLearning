fed50d372d2d25082d75c604bbd92da7429afa8f
Ricardo Pan <3433878731@qq.com>
2024-10-06 16:11:51 +0800

    Chap03

--- AndroidFridaBeginnersBook/Chap03/4.js
+++ AndroidFridaBeginnersBook/Chap03/4.js
@@ -19,8 +19,6 @@ function getTotalValue(){
        // console.log("Inside java perform function")
         var MainAcitivity = Java.use('com.roysue.demo02.MainActivity')
     
-         
-
         // 动态函数主动调用
         Java.choose('com.roysue.demo02.MainActivity',{
             onMatch: function(instance){
